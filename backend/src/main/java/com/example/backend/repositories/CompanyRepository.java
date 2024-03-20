841593319b580efef3b6d6931db935d5fbb3f957
Lucas Soares <soares0lucas8@gmail.com>
2024-03-19 21:44:38 -0300

    feat: adicionando CompanyRepository

---
+++ backend/src/main/java/com/example/backend/repositories/CompanyRepository.java
@@ -0,0 +1,14 @@
+package com.example.backend.repositories;
+
+import com.example.backend.entities.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
+
+    Optional<Company> findByName(String name);
+}
