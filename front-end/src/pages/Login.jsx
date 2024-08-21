1d45a79d7604a92acb4c632bc26fb0b08b4085cc
Livian Torres <liviantorres@alu.ufc.br>
2024-08-21 13:09:45 -0300

    Resolve #31

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,13 @@
 import Forms from "../components/Forms";
 
+
+const handleButtonClick = () => {
+  alert("Botão clicado!");
+  
+};
+
 const Login = () => {
-  return <Forms type="Login" />;
+  return <Forms type="Login" acao={handleButtonClick} />;
 };
 
 export default Login;
--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Forms from "../components/Forms";
+import axios from "axios";
 
 const Register = () => {
+  const [errors, setErrors] = useState([]);
+
+  const handleRegister = async ({ dominio, email, password }) => {
+    setErrors([]); 
+
+    try {
+      const response = await axios.post("http://localhost:8080/company", {
+        name: dominio,
+        email,
+        password,
+      });
+
+      if (response.status === 201) {
+        alert("Empresa cadastrada com sucesso!");
+      }
+    } catch (error) {
+      const errorMessages = [];
+      if (error.response && error.response.data) {
+        if (error.response.data.errors) {
+          error.response.data.errors.forEach(err => {
+            errorMessages.push(err.message);
+          });
+        } else {
+          errorMessages.push(error.response.data.message || error.message);
+        }
+      } else {
+        errorMessages.push(error.message);
+      }
+      setErrors(errorMessages);
+    }
+  };
+
   return (
     <div>
-      <Forms type="Register" />
+      <Forms type="Register" onSubmit={handleRegister} errors={errors} />
     </div>
   );
 };
